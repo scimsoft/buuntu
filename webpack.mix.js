8e6ac6e0b46ab2b0064303e89cd6221bb1a849b9
scimsoft <gerrit.sevilla@gmail.com>
2018-07-10 18:13:55 +0200

    fix node config

--- webpack.mix.js
+++ webpack.mix.js
@@ -9,7 +9,10 @@ const { mix } = require('laravel-mix');
  | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
- */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.js('resources/assets/js/app.js', __dirname +'public/js')
+   .sass('resources/assets/sass/app.scss', __dirname +'public/css');
+   */
+mix.js('resources/assets/js/app.js', 'public/js/app.js')
+    .sass('resources/assets/sass/app.scss', 'public/js/app.scss');
